a7d8450c456dbc7c2bf2b447daa2962cbebe8a1e
Sajid Ashraf <sajidashraf.dev@gmail.com>
2025-08-12 21:50:40 +0500

    creating contact form 8

---
+++ contact_form_8.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name:       Contact Form 8
+ * Plugin URI:        https://example.com/plugins/contact-form-8/
+ * Description:       Get information from users through a contact form.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Sajid Ashraf
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       contact-form-8
+ * Domain Path:       /languages
+ * Requires Plugins:  my-plugin, yet-another-plugin
+ */
+
+?>
